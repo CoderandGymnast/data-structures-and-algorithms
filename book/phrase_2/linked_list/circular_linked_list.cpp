d41ce7a545d9908093ef2acca989cbd80108df77
Sonic <HungICT2407@gmail.com>
2021-06-03 21:26:46 +0700

    Insert at the end & begin of Circualar Linked List

--- book/phrase_2/linked_list/circular_linked_list.cpp
+++ book/phrase_2/linked_list/circular_linked_list.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <iostream>
+using namespace std;
+
 struct CLLNode
 {
     int data;
@@ -8,16 +11,25 @@ struct CLLNode
 };
 
 int len(CLLNode *head);
-void print(CLLNode *head);
+void print(CLLNode **head);
+void insertAtEnd(CLLNode **head, int data);
+void insertAtBegin(CLLNode **head, int data);
 
 int main()
 {
-    CLLNode *head = (CLLNode *)malloc(sizeof(CLLNode));
-    head->data = 1;
-    head->next = head;
+    // CLLNode **head = (CLLNode **)malloc(sizeof(CLLNode *));
+    // *head = (CLLNode *)malloc(sizeof(CLLNode));
+    // (*head)->data = 0;
+    // (*head)->next = *head;
 
-    int numNodes = len(head);
-    printf("Length: %d\n", numNodes);
+    // if (*head == NULL) printf("*head is NULL");
+    // else printf("*head is not NULL");
+    // cout << "*head: " << *head << endl;
+    CLLNode **head = (CLLNode **)malloc(sizeof(CLLNode *));
+    *head = NULL;
+    cout << "*head: " << *head << endl;
+    insertAtEnd(head, 1);
+    insertAtBegin(head, -1);
     print(head);
 }
 
@@ -37,14 +49,70 @@ int len(CLLNode *head)
     return count;
 }
 
-void print(CLLNode *head)
+void print(CLLNode **head)
 {
-    CLLNode *curr = head;
+    CLLNode *curr = *head;
     if (head == NULL)
         return;
     do
     {
         printf("%d\n", curr->data);
         curr = curr->next;
-    } while (curr != head);
+    } while (curr != *head);
+}
+
+void insertAtEnd(CLLNode **head, int data)
+{
+    // The point is to use the address of *head.
+
+    CLLNode *curr = *head;
+    CLLNode *newNode = (CLLNode *)malloc(sizeof(CLLNode));
+
+    if (!newNode)
+    {
+        printf("[ERROR]: out of memory");
+        return;
+    }
+
+    newNode->data = data;
+    if (*head == NULL)
+    {
+        *head = newNode;
+        newNode->next = newNode;
+        return;
+    }
+
+    while (curr->next != *head)
+        curr = curr->next;
+
+    newNode->next = *head;
+    curr->next = newNode;
+}
+
+void insertAtBegin(CLLNode **head, int data)
+{
+    CLLNode *currNode = *head;
+    CLLNode *newNode = (CLLNode *)malloc(sizeof(CLLNode));
+
+    if (!newNode)
+    {
+        printf("[ERROR]: out of memory");
+        return;
+    }
+
+    newNode->data = data;
+
+    if (*head == NULL)
+    {
+        *head = newNode;
+        newNode->next = newNode;
+        return;
+    }
+
+    while (currNode->next != *head)
+        currNode = currNode->next;
+
+    newNode->next = *head;
+    currNode->next = newNode;
+    *head = newNode;
 }
