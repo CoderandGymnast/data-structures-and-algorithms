b21c46fc4dd1073f68482df5e335257b56c4e5ff
Sonic <HungICT2407@gmail.com>
2021-06-24 22:12:09 +0700

    [DATA STRUCTURES & ALGORITHMS]: Insert to empty Unrolled Linked List

--- book/phrase_2/linked_list/unrolled_linked_list.cpp
+++ book/phrase_2/linked_list/unrolled_linked_list.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define BLOCK_SIZE 3
+#define BLOCK_SIZE 4 
 
 struct Node {
     int data;
@@ -10,16 +10,27 @@ struct Node {
  
 struct Block
 {
+    int maxNodes;
     int numNodes;
     Node *head;
     Block *next;
 };
 
+void insert(Block** blockHead, int pos, int data); 
 
 
 int main() {
     Block** blockHead = (Block**) malloc(sizeof(Block*));
     *blockHead = NULL;
+    insert(blockHead, 0, 0);
+    insert(blockHead, 2,2);
+}
+
+void print(Block** blockHead) {
+    Block* currHead = *blockHead;
+    while(currHead) { // currHead != null, process the block.
+        // TODO: print list.
+    }
 }
 
 void insert(Block** blockHead, int pos, int data) {
@@ -38,7 +49,12 @@ void insert(Block** blockHead, int pos, int data) {
 
     // 2. Insert to an empty list:
     if(!*blockHead) {
+        if(pos) {
+            printf("[ERROR]: index out of bound - max index: 0");
+            return;
+        }
         *blockHead = (Block*) malloc(sizeof(Block));
+        (*blockHead)->maxNodes = BLOCK_SIZE;  
         (*blockHead)->numNodes = 1;
         (*blockHead)->next = NULL;
         (*blockHead)->head = newNode;
@@ -56,8 +72,23 @@ void insert(Block** blockHead, int pos, int data) {
 
             // 4. Insert to the suitable block:
             // TODO: 
-            // - Insert to the last.
-            // - Index out of bound when the list is empty.
+            // - Insert to the last. (Need to test "index out of bound - max index.")
+            int blockPos = pos - (numScan - BLOCK_SIZE);
+            if(blockPos + 1 > currBlock->numNodes) {
+                printf("[ERROR]: index out of bound - max index: %d", (numScan - BLOCK_SIZE + currBlock->numNodes));
+                return;
+            }
+            if(currBlock->numNodes < currBlock->maxNodes) {
+                int count = 0;
+                Node* currNode = currBlock->head;
+                while(count < blockPos - 1) {
+                    currNode = currNode->next;
+                }
+            } else if(currBlock->numNodes == currBlock->maxNodes) {
+
+
+            }
+
 
             return;
         }
