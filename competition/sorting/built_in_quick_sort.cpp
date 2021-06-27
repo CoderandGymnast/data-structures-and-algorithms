7a6e8ae97e68cae4029b453f410026f08571cf22
Sonic <HungICT2407@gmail.com>
2021-06-27 23:03:47 +0700

    [COMPETITION - ALGORITHM]: Built-in Quicksort

---
+++ competition/sorting/built_in_quick_sort.cpp
@@ -0,0 +1,22 @@
+/* qsort example */
+#include <stdio.h>      /* printf */
+#include <stdlib.h>     /* qsort */
+
+int values[] = { 40, 10, 100, 90, 20, 25 };
+
+/* ASC or DESC:
+* E.g. "a > b", then "[a,b]"
+*/
+int compare (const void * a, const void * b)
+{
+  return -( *(int*)a - *(int*)b ); // DESC.
+}
+
+int main ()
+{
+  int n;
+  qsort (values, 6, sizeof(int), compare);
+  for (n=0; n<6; n++)
+     printf ("%d ",values[n]);
+  return 0;
+}
