db2bcc8394f00af489e0aa349981899c2d394119
Sonic <HungICT2407@gmail.com>
2021-07-07 12:17:08 +0700

    [COMPETITION]: Check subset of vector

--- competition/longest_common_subpath.cpp
+++ competition/longest_common_subpath.cpp
@@ -8,7 +8,25 @@ class Solution {
     public:
 void print(vector<int>p) {
     for(int&n:p) cout << n << " ";
+    cout << endl;
 }
+    bool isSubset(vector<int>m,vector<int>s) {
+       int subSize=s.size();
+        bool status;
+        for(int i=0;i<=m.size()-subSize;++i) {
+            if(m[i]==s[0]) {
+                status=true;
+                for(int j=1;j<subSize;++j) {
+                    if(m[i+j]!=s[j]) {
+                        status=false;
+                        break;
+                    }
+                }
+                if(status) return true;
+            }
+        }
+        return false;
+    }
 public:
     int longestCommonSubpath(int n, vector<vector<int>>& paths) {
         int minID=-1,minNumCities=INT_MAX,size;
@@ -24,14 +42,9 @@ minID=i;
 for(int l=minNumCities;l>=1;--l)        {
 for(int s=0;s<=minNumCities-l;s++) {
     status=true;
+vector<int>sub(paths[minID].begin()+s,paths[minID].begin()+s+l);
     for(vector<int>p:paths) {
-vector<int> x(paths[minID].begin()+s,paths[minID].begin()+s+l);
-       vector<int>y={2,3};
-
-        
-        
-        
-        if(!includes(p.begin(),p.end(),paths[minID].begin()+s,paths[minID].begin()+s+l)) {
+        if(!isSubset(p,sub)) {
             status=false;
        break; 
         }
@@ -48,15 +61,7 @@ return 0;
 
 int main() {
     Solution s;
-    vector<vector<int>> paths ={
-        {0,1,2,3,4},
-        {5,6,7},
-        {8,9,100,3}
-    };
+    vector<vector<int>> paths ={{0,1,2,3,4},{2,3,4},{4,0,1,2,3}};
 int ans=    s.longestCommonSubpath(5,paths);
 cout << ans;
-vector<int> temp={100,3};
-cout << includes(paths[2].begin(),paths[2].end(),temp.begin(),temp.end());
-s.print(paths[2]);
-s.print(temp);
 }
